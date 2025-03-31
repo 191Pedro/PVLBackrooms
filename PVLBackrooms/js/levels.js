2ce6bfc3bc0cb28e4943992d44a2cd34a377cf1c
Pedro Henrique Custódio <pedropedroka130608@gmail.com>
2025-03-30 23:09:33 -0300

    Design da seleção de nível + barra de pesquisa funcional + responsividade disso em 90%

---
+++ PVLBackrooms/js/levels.js
@@ -0,0 +1,43 @@
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('input', (event) => {
+    const value = formatString(event.target.value);
+
+    const items = document.querySelectorAll('.items .item');
+    const noResults = document.getElementById('no_results');
+    let hasResults = false;
+
+    console.log(items)
+
+    if (value !== '') {
+        items.forEach(item => {
+
+            if (formatString(item.textContent).indexOf(value) !== -1) {
+                item.style.display = 'flex';
+
+                hasResults = true;
+            }
+            else {
+                item.style.display = 'none';
+            }
+        });
+
+        if (hasResults) {
+            noResults.style.display = 'none';
+        } else {
+            noResults.style.display = 'block';
+        }
+
+    } else {
+        items.forEach(item => item.style.display = 'flex');
+        noResults.style.display = 'none';
+    }
+});
+
+function formatString(value) {
+    return value
+        .trim()
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+}
